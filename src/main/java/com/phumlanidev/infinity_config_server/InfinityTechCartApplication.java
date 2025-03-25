366b36f82de954244ea66c0f43a79643b897f6b3
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-03-25 18:10:23 +0200

    initial commit

---
+++ src/main/java/com/phumlanidev/infinity_config_server/InfinityTechCartApplication.java
@@ -0,0 +1,13 @@
+package com.phumlanidev.infinity_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityTechCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityTechCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phumlanidev/infinity_config_server/InfinityTechCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phumlanidev.infinity_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityTechCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
