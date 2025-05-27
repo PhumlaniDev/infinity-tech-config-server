a136a63b52f62c1a178d79731cdc83470af4441a
Phumlani Arendse <aphumlani.dev@gmail.com>
2025-05-27 15:46:57 +0200

    add placeholder comments for documentation in ConfigServerApplication

--- src/main/java/com/phumlanidev/configserver/ConfigServerApplication.java
+++ src/main/java/com/phumlanidev/configserver/ConfigServerApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Comment: this is the placeholder for documentation.
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+  /**
+   * Comment: this is the placeholder for documentation.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
 
 }
